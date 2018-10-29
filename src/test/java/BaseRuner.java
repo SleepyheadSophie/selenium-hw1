77407dd1e4f605b860c476d5139a7df25060608c
Sophie <32652902+SleepyheadSophie@users.noreply.github.com>
2018-10-29 14:02:47 +0300

    Добавлены BaseRuner и BrowserFactory

---
+++ src/test/java/BaseRuner.java
@@ -0,0 +1,24 @@
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class BaseRuner {
+    WebDriver driver;
+    String baseUrl;
+
+    @Before
+    public void setUp() {
+        driver = BrowsersFactory.chrome.create();
+        //driver.manage().window().maximize();
+        baseUrl = "https://moscow-job.tinkoff.ru/";
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+}
